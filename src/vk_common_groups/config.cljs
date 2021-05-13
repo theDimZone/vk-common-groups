236410622f73e17d8e58f5527d72db3e852121cc
Dima Kabarukhin <thedimzone99@gmail.com>
2021-05-13 13:08:13 +0300

    added 'offline' scope for auth url

--- src/vk_common_groups/config.cljs
+++ src/vk_common_groups/config.cljs
@@ -6,7 +6,7 @@
   ^boolean goog.DEBUG)
 
 (def vk-auth-url 
-  "https://oauth.vk.com/authorize?client_id=7846513&redirect_uri=http://127.0.0.1:8280/auth&response_type=token&scope=262146")
+  "https://oauth.vk.com/authorize?client_id=7846513&redirect_uri=http://127.0.0.1:8280/auth&response_type=token&scope=327682")
 
 (def vk-api-url
   (goog.string.Const.from "https://api.vk.com/method/%{method}"))
